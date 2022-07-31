49830f80e01e3c83ae33cb0353f3376a617784a8
Anum Khalid <66093451+byanum@users.noreply.github.com>
2022-07-31 23:07:27 +0500

    Authentication completed

--- uberEats/App.js
+++ uberEats/App.js
@@ -2,8 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 
 import RootNavigator from "./src/navigation";
+import { withAuthenticator } from "aws-amplify-react-native"; //higher order //1 (Auth)
+import Amplify from "aws-amplify"; //Amazon (1)
+import config from "./src/aws-exports"; //Amazon (2)
 
-export default function App() {
+Amplify.configure(config); //configure aws  //Amazon (3)
+
+function App() {
   return (
     <NavigationContainer>
       {/* <RestaurantItem restaurant={restaurants[0]} />
@@ -26,3 +31,5 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+export default withAuthenticator(App); //2 (Auth)
