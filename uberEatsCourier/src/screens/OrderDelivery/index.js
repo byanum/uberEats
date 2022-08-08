8dfb20dab8374aa66306ad2b5a4c15379a8937e6
Anum Khalid <66093451+byanum@users.noreply.github.com>
2022-08-08 20:15:10 +0500

    Order Delivery Screen

--- uberEatsCourier/src/screens/OrderDelivery/index.js
+++ uberEatsCourier/src/screens/OrderDelivery/index.js
@@ -2,6 +2,12 @@ import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import BottomSheet from "@gorhom/bottom-sheet";
 import { useRef, useMemo } from "react";
+import { FontAwesome5, Fontisto } from "@expo/vector-icons";
+import orders from "../../../assets/data/orders.json";
+import styles from "./styles";
+
+const order = orders[0];
+
 const OrderDelivery = () => {
   const bottomSheetRef = useRef(null);
 
@@ -9,32 +15,90 @@ const OrderDelivery = () => {
   const snapPoints = useMemo(() => ["12%", "95%"], []);
   return (
     <View style={styles.container}>
-      <BottomSheet ref={bottomSheetRef} snapPoints={snapPoints}>
-        <Text>OrderDelivery</Text>
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={snapPoints}
+        handleIndicatorStyle={{ backgroundColor: "grey", width: 100 }}
+      >
+        <View style={styles.innerContainer}>
+          <Text style={styles.innerTextA}>14 min</Text>
+          {/* icon */}
+
+          <FontAwesome5
+            name="shopping-bag"
+            size={30}
+            color="lightgreen"
+            style={{ marginHorizontal: 5 }}
+          />
+          <Text style={styles.innerTextB}>5 km</Text>
+        </View>
+
+        <View style={styles.textContainer}>
+          <Text style={styles.name}>{order.Restaurant.name}</Text>
+          <View style={{ flexDirection: "row", padding: 5 }}>
+            <Fontisto
+              name="shopping-store"
+              size={20}
+              color="#3F4E4F"
+              style={{ marginHorizontal: 5 }}
+            />
+
+            <Text style={styles.address}>{order.Restaurant.address}</Text>
+          </View>
+
+          <View style={{ flexDirection: "row", padding: 5 }}>
+            <FontAwesome5
+              name="map-marker-alt"
+              size={20}
+              color="#3F4E4F"
+              style={{ marginHorizontal: 5 }}
+            />
+            <Text style={styles.userAddress}>{order.User.address}</Text>
+          </View>
+        </View>
+
+        <View style={styles.items}>
+          <Text
+            style={{
+              fontSize: 18,
+              color: "grey",
+              fontWeight: "500",
+              letterSpacing: 0.5,
+              marginBottom: 5,
+            }}
+          >
+            Big Mac x3
+          </Text>
+          <Text
+            style={{
+              fontSize: 18,
+              color: "grey",
+              fontWeight: "500",
+              letterSpacing: 0.5,
+              marginBottom: 5,
+            }}
+          >
+            Coco Cola x3
+          </Text>
+          <Text
+            style={{
+              fontSize: 18,
+              color: "grey",
+              fontWeight: "500",
+              letterSpacing: 0.5,
+              marginBottom: 5,
+            }}
+          >
+            McFlurry Strawberry Ice Cream x3
+          </Text>
+        </View>
+        {/* button */}
+        <View style={styles.btn}>
+          <Text style={styles.btnText}>Accept Order</Text>
+        </View>
       </BottomSheet>
     </View>
   );
 };
 
 export default OrderDelivery;
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "lightblue",
-    flex: 1,
-  },
-  innerContainer: {
-    flex: 1,
-    alignItems: "center",
-  },
-  textA: {
-    fontSize: 20,
-    fontWeight: "600",
-    letterSpacing: 0.5,
-    paddingBottom: 5,
-  },
-  textDB: {
-    letterSpacing: 0.5,
-    color: "grey",
-  },
-});
---
+++ uberEatsCourier/src/screens/OrderDelivery/styles.js
@@ -0,0 +1,76 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: "lightblue",
+    flex: 1,
+  },
+  innerContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+  textA: {
+    fontSize: 20,
+    fontWeight: "600",
+    letterSpacing: 0.5,
+    paddingBottom: 5,
+  },
+  textDB: {
+    letterSpacing: 0.5,
+    color: "grey",
+  },
+  innerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+  },
+  innerTextA: {
+    fontSize: 25,
+    letterSpacing: 1,
+  },
+  innerTextB: {
+    fontSize: 25,
+    letterSpacing: 1,
+  },
+  name: {
+    fontSize: 25,
+    letterSpacing: 1,
+    paddingVertical: 20,
+  },
+  address: {
+    fontSize: 20,
+    color: "grey",
+    fontWeight: "500",
+    letterSpacing: 0.5,
+  },
+  userAddress: {
+    fontSize: 20,
+    color: "grey",
+    fontWeight: "500",
+    letterSpacing: 0.5,
+  },
+  items: {
+    borderTopWidth: 1,
+    borderColor: "lightgrey",
+    marginTop: 10,
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+  textContainer: {
+    paddingHorizontal: 20,
+  },
+  btn: {
+    backgroundColor: "#7DCE13",
+    marginTop: "auto",
+    padding: 10,
+    margin: 20,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  btnText: {
+    fontSize: 18,
+    fontWeight: "400",
+    color: "white",
+  },
+});
